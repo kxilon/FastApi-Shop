9bd4e03983c778cc0c176373736e44bfaa322d68
Ksilon <Kustarev2003@mail.ru>
2026-01-05 18:18:46 +0300

    main, run

--- backend/app/schemas/category.py
+++ backend/app/schemas/category.py
@@ -12,5 +12,5 @@ class CategoryResponse(CategoryBase):
     id: int = Field(..., description="Unique category identifier")
 
     class Config:
-        form_attributes = True
+        from_attributes = True
 
